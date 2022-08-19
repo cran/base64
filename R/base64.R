3f9d250b4f5a8554a04c3fa3e1fab8f0db268f9e
Jeroen Ooms <jeroen@berkeley.edu>
2022-08-19 11:40:02 +0000

    version 2.0.1

--- R/base64.R
+++ R/base64.R
@@ -1,15 +1,15 @@
 #' Encode and Decode base64
 #'
-#' Wraps \code{\link[openssl:base64_encode]{openssl::base64_encode}}
-#' to replace the deprecated implementation by Romain Francois.
+#' Wrapper for [openssl::base64_encode].
 #'
 #' @export
 #' @rdname base64
+#' @name base64
 #' @aliases base64
 #' @param input input file
 #' @param output output file
 #' @param linebreaks insert linebreaks to make output human readable
-#' See \code{\link[openssl:base64_encode]{openssl::base64_encode}}
+#' See [openssl::base64_encode]
 #' @importFrom openssl base64_encode
 #' @examples # encode a file
 #' myfile <- R.home("COPYING")
@@ -42,13 +42,13 @@ decode <- function(input, output = tempfile()){
 #' Encode a png file as a img data uri
 #'
 #' This creates html code to embed a png file into an html document.
-#' \Sexpr[results=rd, stage=build, echo=FALSE]{
+#' \if{html}{\Sexpr[results=rd, stage=build, echo=FALSE]{
 #'   pngfile <- tempfile()
 #'   png(pngfile, width = 600, height = 400 )
 #'   plot(1:100, rnorm(100), pch = 21, bg = "red", cex = 2 )
 #'   dev.off()
 #'   base64::img( pngfile, Rd = TRUE )
-#'}
+#'}}
 #'
 #' @export
 #' @importFrom openssl base64_encode
@@ -65,7 +65,7 @@ img <- function( file, Rd = FALSE, alt = "image" ){
   input <- normalizePath(file, mustWork = TRUE)
   buf <- readBin(input, raw(), file.info(input)$size)
   base64 <- base64_encode(buf, linebreaks = FALSE)
-  sprintf('%s<img src="data:image/png;base64,\n%s" alt="%s" />%s',
+  sprintf('%s<img src="data:image/png;base64,%s" alt="%s" />%s',
     if( Rd ) "\\out{" else "", base64, alt, if( Rd ) "}" else ""
   )
 }
